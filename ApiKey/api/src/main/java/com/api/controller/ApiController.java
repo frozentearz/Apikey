b96bc85209f88757850f282467c6e1119376cd09
frazier.xiao <frazier.xiao@tyj365.com>
2019-01-29 16:09:46 +0800

    修改controller名

--- ApiKey/api/src/main/java/com/api/controller/ApiController.java
+++ ApiKey/api/src/main/java/com/api/controller/ApiController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.api.controller.ApiController;
 import com.api.model.pojo.Apikey;
 import com.api.service.IApikeyService;
 
